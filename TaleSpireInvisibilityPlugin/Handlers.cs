668e2d5f26ca1ee7f2442d79a7e4b5cb51513dd7
Lord Ashes <webtable@gmail.com>
2021-08-21 14:29:13 -0400

    Version 1.0.0

---
+++ TaleSpireInvisibilityPlugin/Handlers.cs
@@ -0,0 +1,119 @@
+﻿using BepInEx;
+
+using UnityEngine;
+
+namespace LordAshes
+{
+    public partial class InvisibilityPlugin : BaseUnityPlugin
+    {
+
+        public enum VisibleState
+        { 
+            visibleAll = 0,
+            visibleOwnerOrGM = 1,
+            visibleNot = 2
+        }
+
+        /// <summary>
+        /// Handler for Stat Messaging subscribed messages.
+        /// </summary>
+        /// <param name="changes"></param>
+        public void HandleRequest(StatMessaging.Change[] changes)
+        {
+            foreach (StatMessaging.Change change in changes)
+            {
+                Debug.Log("Change For " + change.cid + ", Type: " + change.action + ", From: " + change.previous + ", To: " + change.value);
+                ApplyVisibility(change);            
+            }
+        }
+
+        private void ApplyVisibility(StatMessaging.Change change)
+        {
+            CreatureBoardAsset asset;
+            CreaturePresenter.TryGetAsset(change.cid, out asset);
+            if (asset != null)
+            {
+                UnityEngine.Debug.Log("Invisibility Plugin: Creature : " + asset.Creature.Name + " : " + asset.Creature.CreatureId +" : Controlled? " + LocalClient.HasControlOfCreature(change.cid));
+
+                VisibleState visible = VisibleState.visibleNot;
+                if ((change.action == StatMessaging.ChangeType.removed))
+                {
+                    //
+                    // Invisibility turned off - Visible to all
+                    //
+                    visible = VisibleState.visibleAll;
+                    if (invisible.ContainsKey(change.cid))
+                    {
+                        // Restore base loader texture
+                        Debug.Log("Invisibility Plugin: Restoring Base Loader Texture");
+                        asset.BaseLoader.LoadedAsset.GetComponent<MeshRenderer>().material.mainTexture = invisible[change.cid];
+                        // Remove mini from the invisible list
+                        Debug.Log("Invisibility Plugin: Removing Mini From Invisibility List");
+                        invisible.Remove(change.cid);
+                    }
+                }
+                else
+                {
+                    //
+                    // Invisibility turned on - Visible to owner and GM 
+                    //
+                    if (LocalClient.HasControlOfCreature(change.cid))
+                    {
+                        visible = VisibleState.visibleOwnerOrGM;
+                    }
+                    if (!invisible.ContainsKey(change.cid))
+                    {
+                        // Save base loader texture
+                        Debug.Log("Invisibility Plugin: Storing Base Loader Texture");
+                        invisible.Add(change.cid, asset.BaseLoader.LoadedAsset.GetComponent<MeshRenderer>().material.mainTexture);
+                        // Apply invisibility texture to the base
+                        Debug.Log("Invisibility Plugin: Changing Base Loader Texture");
+                        asset.BaseLoader.LoadedAsset.GetComponent<MeshRenderer>().material.mainTexture = FileAccessPlugin.Image.LoadTexture(InvisibilityPlugin.Guid + "\\Invisibility.png");
+                    }
+                }
+
+                Debug.Log("Invisibility Plugin: Getting Renderer");
+                Renderer rend = asset.CreatureLoaders[0].GetComponentInChildren<MeshRenderer>();
+                if (rend == null) { rend = asset.CreatureLoaders[0].GetComponentInChildren<SkinnedMeshRenderer>(); }
+                if (rend != null)
+                {
+                    UnityEngine.Debug.Log("Invisibility Plugin: Turned " + rend.GetType().ToString() + " To " + visible);
+                    rend.enabled = (visible == VisibleState.visibleNot) ? false : true;
+                    rend = asset.BaseLoader.GetComponentInChildren<MeshRenderer>();
+                    rend.enabled = (visible == VisibleState.visibleNot) ? false : true;
+                }
+                else
+                {
+                    Debug.Log("Invisibility Plugin: Could Not Find Renderer To Turn On For " + asset.Creature.Name);
+                }
+
+                Debug.Log("Invisibility Plugin: Getting Indicator Renderer");
+                MeshRenderer[] comps = asset.BaseLoader.GetComponentsInChildren<MeshRenderer>();
+                foreach (MeshRenderer obj in comps)
+                {
+                    if(obj.name=="Indicator")
+                    {
+                        obj.enabled = (visible == VisibleState.visibleNot) ? false : true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for Radial Menu selections
+        /// </summary>
+        /// <param name="cid"></param>
+        private void SetRequest(CreatureGuid cid)
+        {
+            if (!invisible.ContainsKey(cid))
+            {
+                StatMessaging.SetInfo(cid, InvisibilityPlugin.Guid, "Invisible");
+            }
+            else
+            {
+                StatMessaging.ClearInfo(cid, InvisibilityPlugin.Guid);
+            }
+        }
+    }
+}
---
+++ TaleSpireInvisibilityPlugin/InvisibilityPlugin.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BepInEx;
+using BepInEx.Configuration;
+using Bounce.Unmanaged;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// Notes:
+/// 
+/// 1. Try to keep this main page simple with just the Unity/TS methods like Awake() and Update() methods
+/// 2. Place your processing code in class specific files or a general class file like the sample Handler.cs file
+/// 3. You can make your additional class file part of the main plugin class by making them "public partial class TemplatePlugin : BaseUnityPlugin"
+///    (See the sample Handler.cs and/or Unity.cs files for an example)
+/// 
+/// </summary>
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, Name, Version)]
+    [BepInDependency(LordAshes.FileAccessPlugin.Guid)]
+    [BepInDependency(LordAshes.StatMessaging.Guid)]
+    [BepInDependency(RadialUI.RadialUIPlugin.Guid)]
+    public partial class InvisibilityPlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Name = "Invisibility Plug-In";             
+        public const string Guid = "org.lordashes.plugins.invisibility";
+        public const string Version = "1.0.0.0";                        
+
+        // Configuration
+        private ConfigEntry<KeyboardShortcut> triggerInvisibility { get; set; }
+        private ConfigEntry<KeyboardShortcut> triggerRefresh { get; set; }
+
+        // Invisible Assets
+        private Dictionary<CreatureGuid,Texture> invisible = new Dictionary<CreatureGuid,Texture>();
+
+        private NGuid _radialTarget = NGuid.Empty;
+
+        private bool subscribed = false;
+
+        /// <summary>
+        /// Function for initializing plugin
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Invisibility Plugin: I'm Ready To Make Minis Disappear.");
+
+            triggerInvisibility = Config.Bind("Hotkeys", "Toggle Invisibility", new KeyboardShortcut(KeyCode.I, KeyCode.LeftControl));
+            triggerRefresh = Config.Bind("Hotkeys", "Refresh", new KeyboardShortcut(KeyCode.I, KeyCode.RightControl));
+
+            if (Config.Bind("Settings", "Move Original Hide To GM Menu", false).Value)
+            {
+                // Remove core TS Hide menu in the root character radial menu
+                RadialUI.RadialUIPlugin.AddOnRemoveCharacter(InvisibilityPlugin.Guid + ".hideGMHide", "Hide");
+
+                // Add Hide menu that does original hide functionality to the GM menu
+                RadialUI.RadialUIPlugin.AddOnSubmenuGm(InvisibilityPlugin.Guid + ".addGMHide", new MapMenu.ItemArgs
+                {
+                    Title = "GM Hide",
+                    Icon = FileAccessPlugin.Image.LoadSprite("Hide.png"),
+                    Action = (mmi2, obj2) =>
+                    {
+                        CreatureBoardAsset asset;
+                        CreaturePresenter.TryGetAsset(new CreatureGuid(_radialTarget), out asset);
+                        Debug.Log("GM Hiding " + asset.Creature.Name + " (" + asset.Creature.CreatureId + ")");
+                        if (asset != null) { CreatureManager.SetCreatureExplicitHideState(asset.Creature.CreatureId, !asset.Creature.IsExplicitlyHidden); }
+                    },
+                    CloseMenuOnActivate = true
+                }, (m, r) => { _radialTarget = r; return true; });
+            }
+
+            // Add replacement Hide menu in the root character radial menu
+            RadialUI.RadialUIPlugin.AddOnCharacter(InvisibilityPlugin.Guid + ".addPCHide", new MapMenu.ItemArgs
+            {
+                Title = "PC Hide",
+                Icon = FileAccessPlugin.Image.LoadSprite("Hide.png"),
+                Action = (mmi1, obj1) =>
+                {
+                    CreatureBoardAsset asset;
+                    CreaturePresenter.TryGetAsset(new CreatureGuid(_radialTarget), out asset);
+                    Debug.Log("Player Hiding " + asset.Creature.Name + " (" + asset.Creature.CreatureId + ")");
+                    if (asset != null) { SetRequest(asset.Creature.CreatureId); }
+                },
+                CloseMenuOnActivate = true
+            },(m,r)=> { _radialTarget = r; return true; });
+
+            Utility.PostOnMainPage(this.GetType());
+        }
+
+        /// <summary>
+        /// Function for determining if view mode has been toggled and, if so, activating or deactivating Character View mode.
+        /// This function is called periodically by TaleSpire.
+        /// </summary>
+        void Update()
+        {
+            if (Utility.isBoardLoaded())
+            {
+                if (!subscribed)
+                {
+                    Debug.Log("Invisibility Plugin: Subscribing To Invisibility Plugin Stat Messages...");
+                    subscribed = true;
+                    StatMessaging.Subscribe(InvisibilityPlugin.Guid, HandleRequest);
+                }
+
+                if (Utility.StrictKeyCheck(triggerInvisibility.Value))
+                {
+                    Debug.Log("Toggling Invisibility State...");
+                    if (LocalClient.SelectedCreatureId != null)
+                    {
+                        SetRequest(LocalClient.SelectedCreatureId);
+                    }
+                }
+
+                if (Utility.StrictKeyCheck(triggerRefresh.Value))
+                {
+                    Debug.Log("Refreshing Invisibility States...");
+                    foreach (CreatureBoardAsset asset in CreaturePresenter.AllCreatureAssets)
+                    {
+                        ApplyVisibility(new StatMessaging.Change()
+                        {
+                            cid = asset.Creature.CreatureId,
+                            action = (invisible.ContainsKey(asset.Creature.CreatureId)) ? StatMessaging.ChangeType.modified : StatMessaging.ChangeType.removed,
+                            key = InvisibilityPlugin.Guid,
+                            value = "Refresh:" + DateTime.UtcNow
+                        }); ;
+                    }
+                }
+            }
+        }
+
+        private void PlayerHide(CreatureGuid cid, string txt, MapMenuItem mmi)
+        {
+            CreatureBoardAsset asset;
+            CreaturePresenter.TryGetAsset(new CreatureGuid(RadialUI.RadialUIPlugin.GetLastRadialTargetCreature()), out asset);
+            Debug.Log("Player Hiding " + asset.Creature.Name + " (" + asset.Creature.CreatureId + ")");
+            if (asset != null) { SetRequest(asset.Creature.CreatureId); }
+        }
+
+        private void GMHide(CreatureGuid cid, string txt, MapMenuItem mmi)
+        {
+            CreatureBoardAsset asset;
+            CreaturePresenter.TryGetAsset(new CreatureGuid(RadialUI.RadialUIPlugin.GetLastRadialTargetCreature()), out asset);
+            Debug.Log("GM Hiding " + asset.Creature.Name + " (" + asset.Creature.CreatureId + ")");
+            if (asset != null) { CreatureManager.SetCreatureExplicitHideState(asset.Creature.CreatureId, !asset.Creature.IsExplicitlyHidden); }
+        }
+    }
+}
